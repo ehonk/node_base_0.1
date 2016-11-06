09baa88007f2c9dde6451d6c160bb89bfafe2b1b
Andreas <am@gmx.de>
2016-11-06 19:15:59 +0000

    jquery and angular implemented

--- public/javascripts/codeIndex.js
+++ public/javascripts/codeIndex.js
@@ -4,5 +4,8 @@ function Index_Load()
 {
     //console.info ("## JQuery Core: " + $.fn.jquery + " JQuery UI " + $.ui.version);
 	console.info ("## Index_Load" );
+	console.info ("## JQuery Core: " + $.fn.jquery);
+	console.info ("## angular : " + angular.version.full);
+
 
 }
