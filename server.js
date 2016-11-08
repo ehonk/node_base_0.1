9a0c5fa8a38f5f20806c7174666ae196ebe762ab
Andreas <am@gmx.de>
2016-11-08 22:20:55 +0000

    mongoose and mongo

--- server.js
+++ server.js
@@ -6,6 +6,8 @@
     var express  = require('express');
     var app      = express();                               // create our app w/ express
     var mongoose = require('mongoose');                     // mongoose for mongodb
+    var mongodb = require('mongodb');                       // pure mongo
+    var MongoClient = mongodb.MongoClient;
 
     // Express4 Special
     var morgan = require('morgan');             // log requests to the console (express4)
@@ -14,21 +16,70 @@
 
     // configuration =================
 
+    // Mongoose Block
+
+
     //mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
     mongoose.connect('mongodb://localhost/test');
     var db = mongoose.connection;
-    //db.on('error', console.error.bind(console, 'connection error:'));
-
-
-   db.on('error', function() {
+     db.on('error', function() {
         // we're connected!
         console.log (" mongoose we're NOT connected!");
     });
-
     db.once('open', function() {
         // we're connected!
         console.log (" mongoose we're connected!");
+
+        var Schema = mongoose.Schema;
+
+        // create a schema
+        var userSchema = new Schema({
+             name: String
+        });
+        var User = mongoose.model('movie', userSchema);
+
+        // get all the users
+        User.find({}, function(err, users) {
+        if (err) throw err;
+
+        // object of all the users
+        console.log(users);
+        });
+
+        // get the user starlord55
+    User.find({ username: 'tutorials point' }, function(err, user) {
+    if (err) throw err;
+
+    // object of the user
+    console.log("Mongo Object : " + user);
     });
+
+
+    });
+
+     // Mongoose Block
+    
+
+    // Mongo Block
+/*
+    var url = 'mongodb://localhost:27017/test'; // Connection URL. This is where your mongodb server is running.
+    MongoClient.connect(url, function (err, db) { // Use connect method to connect to the Server
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      console.log('< Mongo > Connection established to', url);
+      var collection = db.collection('movie');         // Get the documents collection
+      var cursor = collection.find({name: 'tutorials point' });        //We have a cursor now with our find criteria
+      cursor.each(function (err, doc) {             //Lets iterate on the result
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Fetched:', doc);
+      }
+    });
+  }
+  }); */
+ // Mongo Block
 
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
